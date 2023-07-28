a2957f8ce0288b3d8ad005769aca893c828ea687
jfelipeforero <felipe__2001@hotmail.com>
2023-07-28 17:26:07 -0500

    Add issue report and confirm dialog components

---
+++ src/app/issue-list/issue-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { IssuesService } from '../issues.service';
+import { Issue } from '../issue';
+
+@Component({
+  selector: 'app-issue-list',
+  templateUrl: './issue-list.component.html',
+  styleUrls: ['./issue-list.component.css']
+})
+
+export class IssueListComponent implements OnInit{
+  issues: Issue[] = [];
+  showReportIssue = false;
+  constructor(private issueService: IssuesService) {}
+  ngOnInit(): void {
+      this.getIssues();
+  }
+
+  private getIssues() {
+    this.issues =  this.issueService.getPendingIssues();
+  }
+
+  onCloseReport() {
+    this.showReportIssue = false;
+    this.getIssues;
+  }
+}
